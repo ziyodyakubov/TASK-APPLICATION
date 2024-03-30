b9f1e2ae4ebb3f2761f1ca22bebd69cead558fa4
ziyodyakubov <ziyodyakubov23@gmail.com>
2024-03-31 01:48:02 +0500

    ui ended

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import './App.css'
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const [value,inputValue] = useState("");
 
   return (
     <>
     <main class="container flex flex-direction">
         <form action="">
           <div className="form-wrapper">
-            <input type="text" required placeholder='Enter a title'/>
+            <input type="text" onChange={()=>inputValue(value)} required placeholder='Enter a title'/>
             <textarea name="" id="" cols="30" rows="10" required placeholder='Enter a description'></textarea>
 
             <div className="time-wrapper">
@@ -34,6 +36,16 @@ export function App() {
                   <h3>Description:</h3>
                   <p>Read more 20 pages than yesterday</p>
                 </div>
+
+                <div className="deadline-time">
+                  <h3>Deadline: </h3>
+                  <h2><span class="dating">04.05.2024 </span> <span class="timing">08:00</span></h2>
+                </div>
+            </div>
+
+            <div className="right">
+              <button class="delete-btn">Delete <i class='bx bxs-trash'></i></button>
+              <button class="edit-btn">Edit <i class='bx bx-edit-alt'></i></button>
             </div>
             </div>
         </div>
